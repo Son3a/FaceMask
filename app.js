7d93f23e39e822811f1384c6f32d7ad3fef90c97
N19DCCN013@student.ptithcm.edu.vn <N19DCCN013@student.ptithcm.edu.vn>
2023-11-29 03:41:31 +0700

    add nomask

--- app.js
+++ app.js
@@ -6,18 +6,18 @@ const app = express();
 const path = require('path');
 const server = require('http').createServer(app);
 
-// const cloudinary = require('cloudinary')
-// cloudinary.config({
-//     cloud_name: 'dnstykqpa',
-//     api_key: '592721243373484',
-//     api_secret: 'II9-bCcD8CkphOGPwwiClJLx7zQ',
-//     secure: true
-// });
+const cloudinary = require('cloudinary')
+cloudinary.config({
+    cloud_name: 'dnstykqpa',
+    api_key: '592721243373484',
+    api_secret: 'II9-bCcD8CkphOGPwwiClJLx7zQ',
+    secure: true
+});
 
-// var portSerial = new serialport.SerialPort({ path: portName, baudRate: 115200 });
-// portSerial.on('open', () => {
-//     console.log("on opening");
-// })
+var portSerial = new serialport.SerialPort({ path: portName, baudRate: 115200 });
+portSerial.on('open', () => {
+    console.log("on opening");
+})
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -27,15 +27,14 @@ app.use(express.static(__dirname + '/public'));
 
 var io = require('socket.io')(server);
 
-// io.on("connection", function (socket) {
-//     socket.on("lights", function (data) {
-
-//         if (data.result != '') {
-//             cloudinary.uploader.upload(data.result).then((result) => {
-//                 console.log(result.url);
-//                 portSerial.write(result.url)
-//             });
-//         }
-//     });
-// });
+io.on("connection", function (socket) {
+    socket.on("lights", function (data) {
+        if (data.result != '') {
+            cloudinary.uploader.upload(data.result).then((result) => {
+                console.log(data.tag + result.url);
+                portSerial.write(data.tag + result.url)
+            });
+        }
+    });
+});
 server.listen(3000, () => console.log('listening in port 3000'));
